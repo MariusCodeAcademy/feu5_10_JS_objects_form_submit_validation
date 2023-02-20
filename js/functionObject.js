e2f27a6189470b5acfb5ed37770e2b62fe20cee9
Marius CA <marius@example.com>
2023-02-20 10:36:05 +0200

    makeGreeting

--- js/functionObject.js
+++ js/functionObject.js
@@ -6,7 +6,16 @@ const user = {
   town: 'London',
   age: 45,
   isOnline: true,
+  makeGreeting: function () {
+    // makeGreeting yra metodas. metodas yra funkcija objekto viduje
+    // "my name is James and Im from London"
+    // this === user
+    const finalOutput = `my name is ${this.username} and Im from ${this.town}`;
+    console.log('makeGreeting ===', finalOutput);
+  },
 };
+
+user.makeGreeting();
 
 // funkcija kuri atspausdina "my name is James and Im from London"
 
