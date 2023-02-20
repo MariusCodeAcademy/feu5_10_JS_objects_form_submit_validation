f59be73357ed7b0cccf10b17452e971d4f4081a3
Marius CA <marius@example.com>
2023-02-20 11:43:07 +0200

    clearErrors

--- js/formHandle.js
+++ js/formHandle.js
@@ -8,6 +8,8 @@ const formEl = document.getElementById('userForm');
 const emailEl = formEl.querySelector('#email');
 const nameEl = formEl.querySelector('#name');
 const ageEl = formEl.querySelector('#age');
+// errors EL
+const errorsEl = document.getElementById('errors');
 // debugger;
 
 // uzdeti formai ivykio submit pasiklausyma
@@ -19,12 +21,36 @@ function formSubmitHandler(event) {
   // event.preventDefault() - sustabdyti numatytaji veiksma, siuo atveju perkovima ir duomenu issiuntima kitu budu
   // ivykus ivykiui mes sustabdysim forma
   event.preventDefault();
-  console.log('formSubmitHandler ran');
+  console.log('formSubmitHandler ivyko');
+
+  // issivalom pries tai buvusias klaidas
+  clearErrors();
+
+  if (emailEl.value === '' || nameEl.value === '' || ageEl.value === '') {
+    // alert('prasom uzpildyti');
+    showError();
+    return;
+  }
+
   const newUserFromInputs = {
     email: emailEl.value,
-    name: '',
-    age: '',
+    name: nameEl.value,
+    age: ageEl.value,
   };
   console.log('newUserFromInputs ===', newUserFromInputs);
 }
 // sustabde forma nuo perkrovimo mes pasiimsim visu inputu reikmes
+
+// jei reiksme neivesta, pranesam apie klaida
+
+// function pranesti apie klaida
+function showError() {
+  const h2El = document.createElement('h2');
+  h2El.textContent = 'Visi laukai privalomi';
+  h2El.classList.add('singleError');
+  errorsEl.append(h2El);
+}
+
+function clearErrors() {
+  errorsEl.innerHTML = '';
+}
