d74f47da3b8692013963a3bc551f465065579533
Marius CA <marius@example.com>
2023-02-20 10:31:43 +0200

    arrow

--- js/functionExpression.js
+++ js/functionExpression.js
@@ -3,6 +3,10 @@ console.log('functionExpression.js file was loaded');
 
 // sukurti funkcija kuri sudeda 3 reiksmes. atspausdina ir grazina reiksme
 
+// del js hoisting mes galim iskviesti funkcija pries ja deklaruodamis
+add(3, 2, 3);
+
+// function deklaration
 function add(n1, n2, n3) {
   const sum = n1 + n2 + n3;
   console.log('sum ===', sum);
@@ -10,3 +14,31 @@ function add(n1, n2, n3) {
 }
 
 add(1, 2, 3);
+
+// function expression
+// addExpression(10, 20, 30); // kvieciam tik po inicializacijos
+
+const addExpression = function (n1, n2, n3) {
+  const sum = n1 + n2 + n3;
+  console.log('sum expression ===', sum);
+  return sum;
+};
+addExpression(10, 20, 30);
+
+// anonimine arrow funkcija =========================================
+// '=>' tas pats kaip ir 'return'
+const addExpressionArrow = (n1, n2, n3) => {
+  const sum = n1 + n2 + n3;
+  console.log('sum addExpressionArrow ===', sum);
+  return sum;
+};
+addExpressionArrow(5, 5, 5);
+
+// jei musu funkcija iskart grazina reiksme galime ja sutrumpinti i viena eilute
+// const sumArrow = (n1, n2, n3) => {
+//   return n1 + n2 + n3;
+// };
+
+const sumArrow = (n1, n2, n3) => n1 + n2 + n3;
+
+console.log(sumArrow(1, 5, 6));
